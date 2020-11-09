00b6edd7661fa38b59ca55636cd07239898891a3
Jason Shave <jasonshave@hotmail.com>
2020-11-09 09:05:05 -0700

    updated classes to be sealed and created new custom exception

---
+++ SpeechConverter.App/DuplicateFileException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SpeechConverter.App
+{
+    public sealed class DuplicateFileException : Exception
+    {
+        public DuplicateFileException()
+        {
+            
+        }
+
+        public DuplicateFileException(string? message, string? file1, string? file2)
+            : base(message)
+        {
+            
+        }
+
+        public DuplicateFileException(string? message, string? file1, string? file2, Exception? innerException)
+            : base(message, innerException)
+        {
+            
+        }
+    }
+}
